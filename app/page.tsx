71805e74265a315f5ba8475862552e6226904fa8
Antony Lewis <antony@cosmologist.info>
2024-06-06 09:25:42 +0100

    first

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,13 @@ export default function Home() {
     <main>
       <div className="mx-auto mb-12 max-w-lg text-center">
         <div className="m-4">
-          <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">OpenAI Assistant Starter Kit</h1>
+          <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">Crossword Compiler Help</h1>
           <div className="mb-6 text-normal font-normal text-gray-500">
-            This Starter Kit illustrates how you can create an OpenAI Assistant with ReactJS + NextJS + TypeScript.
-            You can <a href="https://github.com/Superexpert/openai-assistant-starter-kit" target="_new" className="underline text-blue-500">download the source code</a> for 
-            this Starter Kit from GitHub. 
           </div>
         </div>
         <OpenAIAssistant 
-          assistantId="asst_gx3Htc0gLVNlpBQKLoefkXZZ"
-          greeting="I am a helpful chat assistant. How can I help you?"
+          assistantId="asst_LozNIVQK0nGDswTpgKlict1B"
+          greeting="How can I help you?"
         />
       </div>
     </main>
